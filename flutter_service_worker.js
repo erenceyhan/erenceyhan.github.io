5a600114ed80bfb7a50616a9fdc63bd8025fe146
Eren <erenceyhann@gmail.com>
2026-04-12 02:44:42 +0300

    Publish Safari native SVG input build

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,14 +23,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "e22653ede92eaa0b547b48b0c8b2bc0c
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "5021ef697017ae8ecac36bd04a1b800b",
+"flutter_bootstrap.js": "cf8adb9a8de84fd4c2971714d0861db9",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "945337c3a4e49dbc7667c10d260e5bee",
 "/": "945337c3a4e49dbc7667c10d260e5bee",
-"main.dart.js": "25350e89f53fe5469144338765fc576d",
+"main.dart.js": "5e12e1401e85e12597a8c894c9ec9a60",
 "manifest.json": "71e0154c18320c9ca4cbf26c3e30935d",
 "version.json": "322dfa94430cc69aa45ce83495f46466"};
 // The application shell files that are downloaded before a service worker can
